a666b32ca97acb637dce8d02e1cc1efe2c07f73f
Ignacio <ignacio.damanes@gmail.com>
2023-08-17 10:27:04 -0400

    version final

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,7 @@ import { Section } from "./../components/section";
 import { Header } from "./../components/header"
 import { Footer } from "./../components/footer";
 import { NavBar } from "./../components/navbar";
+import { Section3 } from "./../components/section-3";
 export default function Home(){
     return (
         <>
@@ -15,23 +16,12 @@ export default function Home(){
         <Section />
         {/*<!-- Content section 2-->*/}
         <Section2 />
-        <Section2 />
+       
         {/*<!-- Content section 3-->*/}
-        <section>
-            <div className="container px-5">
-                <div className="row gx-5 align-items-center">
-                    <div className="col-lg-6 order-lg-2">
-                        <div className="p-5"><img className="img-fluid rounded-circle" src="assets/img/03.jpg" alt="..." /></div>
-                    </div>
-                    <div className="col-lg-6 order-lg-1">
-                        <div className="p-5">
-                            <h2 className="display-4">Let there be rock!</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <Section3 />
+        <Section3 />
+        <Section3 />
+        <Section3 />
 
        {/*<!-- Footer-->*/}
        <Footer />
